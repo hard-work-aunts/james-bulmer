7c8b9af859882920ad89ac6ed250dc2bb163841b
James Bulmer <jabulmer12@James-PC.(none)>
2013-01-20 01:23:47 +0000

    Added variables for store count

--- Small Scripts/xml.php
+++ Small Scripts/xml.php
@@ -13,8 +13,13 @@ foreach ($xml->state as $state) {
 		echo "\t\t\tnew google.maps.LatLng(" . $point->attributes() ->lat->__toString() . ", " . $point->attributes() ->lng->__toString() . "),\n";
 	}
 	echo "\t\t],\n";
-	echo "\t\tstate: '" . str_replace(' ', '_', strtolower($state->attributes() ->name)) . "', \n";
+	echo "\t\tstate: '" . $state->attributes() ->name . "', \n";
 	echo "\t\tstarbucks: 0, \n";
+	echo "\t\tpizzahut: 0, \n";
+	echo "\t\tdunkindonuts: 0, \n";
+	echo "\t\tkfc: 0, \n";
+	echo "\t\tmcdonalds: 0, \n";
+	echo "\t\ttacobell: 0, \n";
 	echo "\t\tfillColor: '#0099cc',\n";
 	echo "\t\tfillOpacity: 0.66,\n";
 	echo "\t\tstrokeWeight: 1,\n";
